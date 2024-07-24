0b07d1e34986187bec8bff76c370808de90d3bbe
roc <sushaopeng@tinyzk.com>
2024-07-24 14:41:48 +0800

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by roc on 2024/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
