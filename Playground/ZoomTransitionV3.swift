9cd2cfb834e3bef787389d3f567a451f8f37d986
roc <sushaopeng@tinyzk.com>
2024-08-01 17:20:52 +0800

    ZoomTransitionV3优化版

--- Playground/ZoomTransitionV3.swift
+++ Playground/ZoomTransitionV3.swift
@@ -8,64 +8,78 @@ struct ZoomTransitionV3: View {
     @State private var selectedId = 1
     @Namespace private var namespace
     @GestureState private var draggingOffset: CGSize = .zero
+    @State private var bgOpacity: Double = 1
     
     var body: some View {
         NavigationStack {
-            ScrollViewReader { scrollView in
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(samplePhotos) { photo in
-                            Image(photo.name)
-                                .resizable()
-                                .frame(width: 100, height: 100)
-                                .offset(photo.id == selectedId ? draggingOffset : .zero)
-                                .onTapGesture {
-                                    isSelected.toggle()
-                                    selectedId = photo.id
-                                }
-                                .matchedGeometryEffect(id: photo.id, in: namespace, isSource: !isSelected && selectedId == photo.id)
-                        }
-                    }
-                }
-                .onChange(of: selectedId) { _, index in
-                    withAnimation {
-                        scrollView.scrollTo(index, anchor: .center)
-                    }
-                }
-            }
-        }
-        .overlay(
-            Group {
-                if isSelected {
-                    ZStack {
-                        TabView(selection: $selectedId) {
+            List {
+                ScrollViewReader { scrollView in
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
                             ForEach(samplePhotos) { photo in
                                 Image(photo.name)
                                     .resizable()
-                                    .frame(width: 200, height: 200)
-                                    .offset(draggingOffset)
-                                    .tag(photo.id)
-                                    .matchedGeometryEffect(id: photo.id, in: namespace, isSource: isSelected && selectedId == photo.id)
+                                    .frame(width: 100, height: 100)
+                                    .offset(photo.id == selectedId ? draggingOffset : .zero)
+                                    .onTapGesture {
+                                        withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.8, blendDuration: 0.8)) {
+                                            isSelected.toggle()
+                                            selectedId = photo.id
+                                        }
+                                    }
+                                    .matchedGeometryEffect(id: photo.id, in: namespace, isSource: !isSelected && selectedId == photo.id)
                             }
                         }
-                        .tabViewStyle(PageTabViewStyle())
+                    }
+                    .onChange(of: selectedId) { _, index in
+                        withAnimation {
+                            scrollView.scrollTo(index, anchor: .center)
+                        }
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("图片转场")
+            .overlay(
+                Group {
+                    if isSelected {
+                        ZStack {
+                            Color.black.opacity(bgOpacity)
+                                .ignoresSafeArea(.all)
+                            TabView(selection: $selectedId) {
+                                ForEach(samplePhotos) { photo in
+                                    Image(photo.name)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .offset(draggingOffset)
+                                        .tag(photo.id)
+                                        .matchedGeometryEffect(id: photo.id, in: namespace, isSource: isSelected && selectedId == photo.id)
+                                }
+                            }
+                            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                        }
                         .gesture(
                             DragGesture()
                                 .updating($draggingOffset) { value, outValue, _ in
                                     outValue = value.translation
+                                    let halfHeight = UIScreen.main.bounds.height / 2
+                                    let progress = abs(draggingOffset.height / halfHeight)
+                                    bgOpacity = Double(1 - progress)
                                 }
                                 .onEnded { value in
                                     let translation = abs(value.translation.height)
                                     if translation > 150 {
-                                        isSelected.toggle()
+                                        withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.8, blendDuration: 0.8)) {
+                                            isSelected.toggle()
+                                        }
                                     }
-                                    
+                                    bgOpacity = 1
                                 }
                         )
                     }
                 }
-            }
-        )
+            )
+        }
     }
 }
 
